c0cbc9f6680c7ee5c908b3702d01cc800d664713
emgordon154 <30507443+emgordon154@users.noreply.github.com>
2018-01-10 14:45:02 -0500

    deleted secrets.js

--- secrets.js
+++ secrets.js
@@ -1,7 +1,1 @@
-//secret
-process.env.SESSION_SECRET = "secrets secrets are no fun unless I am a part of one";
 
-//google secrets
-process.env.GOOGLE_CLIENT_ID = "1051788331583-tb74988bh8m064utl1pi3gtckhc59krm.apps.googleusercontent.com"
-process.env.GOOGLE_CLIENT_SECRET = "pfDrMMCikKEk1tEHPsmAwwcB"
-process.env.GOOGLE_CALLBACK = "http://localhost:8080/auth/google/callback"
