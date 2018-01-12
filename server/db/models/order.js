5b6997c29636aeede2b69a86f8413e2edd0abd97
Natalie Ung <ung.natalie@gmail.com>
2018-01-12 11:21:46 -0500

    orders model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,6 +4,12 @@ const db = require('../db')
 const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM('Created', 'Processing', 'Canceled', 'Completed')
+  },
+  address: {
+    type: Sequelize.STRING
+  }, 
+  subtotal: {
+    type: Sequelize.INTEGER
   }
 })
 
