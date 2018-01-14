e33b1394ab42a2b2ae3fc621421bb61138d6351e
Ruth Hill <ruthhill.e@gmail.com>
2018-01-13 21:15:14 -0500

    [FEATURE] Incomplete - single-review component

---
+++ client/components/single-review.js
@@ -0,0 +1,60 @@
+import React from  'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import {composeReview, submitReview} from '../store/review-form'
+
+const Review = (props) => {
+  const {title, content, rating, handleChange, handleSubmit} = props
+
+  return (
+    <div>
+      <form id="submit-review">
+        <div>
+          <input
+            label="Title"
+            value={title}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <div>
+          <input
+            label="Content"
+            value={content}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <div>
+          <input
+            label="Rating"
+            value={rating}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <button type="Submit" onClick={handleSubmit}>Submit Review</button>
+      </form>
+    </div>
+  )
+}
+
+const mapState = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleChange (event) {
+
+    },
+    handleSubmit (event) {
+      event.preventDefault()
+      dispatch(submitReview(/* Review */))
+    }
+  }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(Review))
